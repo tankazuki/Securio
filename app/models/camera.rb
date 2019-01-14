4c5fab88c38742ef88f75500727f1dd18ca1988d
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-14 17:04:11 +0900

    [Fix] Cameraモデルのバリデーションおよびアソシエーションを追記

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,3 +1,17 @@
 class Camera < ApplicationRecord
   belongs_to :manufacturer
+  has_many :favorites
+  has_many :camera_images
+  has_many :camera_codecs
+  has_many :reviews
+  has_many :reports
+
+  validates :camera_name,  presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :resolution, presence: true
+  validates :camera_type, presence: true
+  validates :audio, presence: true
+  validates :size, presence: true
+  validates :weight, presence: true, :numericality => { :less_than => 100 }
+
+  add_foreign_key :cameras, :manufacturers
 end
