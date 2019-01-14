dab60556df463b6b3f41229097b1b1ea2f841d3c
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-14 16:58:46 +0900

    [Add] Userモデルにバリデーションとアソシエーションを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  has_many: favorites
+  has_many: reports
+  has_many: reviews
+
+  validates :nickname,  presence: true, length: { maximum: 20 }
+  validates :mail, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :password_digest, presence: true, length: { minimum: 20 }
+
 end
