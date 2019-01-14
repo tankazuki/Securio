6279e1227fcf148413c61ce012c1c0fc15e84c5f
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-14 17:00:20 +0900

    [Fix] アソシエーションの結び方を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
-  has_many: favorites
-  has_many: reports
-  has_many: reviews
+  has_many :favorites
+  has_many :reports
+  has_many :reviews
 
   validates :nickname,  presence: true, length: { maximum: 20 }
   validates :mail, presence: true, uniqueness: true, length: { maximum: 20 }
