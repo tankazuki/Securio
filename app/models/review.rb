dad5897941588b647a79482adbd80d7bf0d7c2be
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-14 17:06:43 +0900

    [Add] Reviewモデルのバリデーションおよびアソシエーションを追記

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
+
   belongs_to :camera
   belongs_to :user
+
+  validates :review_text,  presence: true, length: { maximum: 200 }
+
+  add_foreign_key :reviews, :users
+  add_foreign_key :reviews, :cameras
+
 end
