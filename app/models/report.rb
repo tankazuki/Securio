fe68b6eeb9a7d209b2ae17c5805d8c0d4698c055
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-14 17:46:48 +0900

    [Add] Reportモデルのバリデーションとアソシエーションを追記

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,11 @@
 class Report < ApplicationRecord
+
   belongs_to :camera
   belongs_to :user
+
+  validates :report_text, presence: true, length: { maximum: 200 }
+
+  add_foreign_key :reports, :users
+  add_foreign_key :reports, :cameras
+
 end
