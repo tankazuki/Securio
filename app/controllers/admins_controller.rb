e12a541655ea72fae704d1d8a164119b68ee2377
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-17 17:30:31 +0900

    [Add] adminの新規登録ページをおよび処理を記述

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,24 @@
 class AdminsController < ApplicationController
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      #sign_in_admin @admin
+      #flash[:success] = "新規登録が成功しました"
+      redirect_to root_path
+     else
+      render 'new'
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:mail, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'top#top'
+
+  #get    'sign_in/admin', to: 'sessions#admin_new', as: 'sign_in_admin'
+  #post   'sign_in/admin', to: 'sessions#admin_create'
+  #delete 'admin/sign_out', to: 'sessions#admin_destroy', as: 'sign_out_admin'
+  get    'admin/sign_up', to: 'admins#new'
+  post   'admin/sign_up', to: 'admins#create'
+
 end
