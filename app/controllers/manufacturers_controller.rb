b96917f8f99b3a49b3c52638ae2926aaa62cc5cd
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-18 16:37:36 +0900

    [Add] メーカー追加機能を実装

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,2 +1,24 @@
 class ManufacturersController < ApplicationController
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:manufacturer_name)
+  end
 end
