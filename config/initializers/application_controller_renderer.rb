c0bb713f5f6e32208704168722b70975f8b7b5d7
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-20 16:04:12 +0900

    [Add] refile

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'afdaf8037c3a53661b9f3a8bfe4913575e59d5ec2fb366fbacaddc97caa2b6c495690ab80d595ce649a6fd1e43f370a76d9d76c141ca6ffb4d2ea6b7e8cd0ae4'
