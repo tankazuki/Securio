58f17e18522494b9a2144034116680b04573b997
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-21 17:05:51 +0900

    [Add] ログインとログアウトのメソッドを記述

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,4 +22,31 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
 
+  def user_new
+		@user = User.new
+	end
+
+  def user_create
+	user = User.find_by(mail: params[:session][:mail].downcase)
+ 		if user && user.authenticate(params[:session][:password])
+  			if user.enable == true
+      		 sign_in_user user
+				flash.now[:success] = "ログインしました"
+      		 redirect_to root_url
+  			else
+  				flash.now[:danger] = "すでに退会済みユーザーです"
+  				render 'User_new'
+  			end
+  		else
+  			flash.now[:danger] = "入力内容の確認をお願いします"
+  			render 'User_new'
+  		end
+	end
+
+  def user_destroy
+    sign_out_user
+    flash[:success] = "ログアウトしました"
+    redirect_to root_url
+  end
+
 end
