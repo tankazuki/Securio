7e7e39de1b9249488963e8bf6267ac6d1795ee42
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-21 17:06:31 +0900

    [Add] userの新規作成のメソッドを記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  	if @user.save
+      sign_in_user @user
+      #flash[:success] = "新規登録が成功しました"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :mail, :password, :password_confirmation, :user_image_id)
+  end
 end
