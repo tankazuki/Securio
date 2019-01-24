f92c8f01f56884c57e8310526bc3c9b956249bcb
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-24 11:17:00 +0900

    [Add]ニュース記事作成機能を実装

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,21 @@
 class NewsController < ApplicationController
+
+  def new
+    @news = News.new
+  end
+
+  def create
+    @news = News.new(news_params)
+    if @news.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def news_params
+    params.require(:news).permit(:title, :content, :news_type, :news_image)
+  end
+
 end
