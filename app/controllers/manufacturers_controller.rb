55b8b0c148879ecf28fdb84830331e82376e044b
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-01-30 18:12:26 +0900

    [Add] 編集処理を記述

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -13,6 +13,20 @@ class ManufacturersController < ApplicationController
     end
   end
 
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    @manufacturer.update(manufacturer_params)
+    if @manufacturer.save
+      redirect_to admin_manufacturer_index_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @manufacturer = Manufacturer.find(params[:id])
     @manufacturer.destroy
