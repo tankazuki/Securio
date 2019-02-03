f75fed8f41930ee974a333bd0a9f7ff5e889b424
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-02-03 19:00:39 +0900

    [Add &Fix] 不要な記述を削除、お気に入り一覧を返す処理を記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(user_id: params[:user_id], camera_id: params[:camera_id])
     @favorite.save
-    {
-      aa: "aaa",
-      ss: "ss"
-    }
     render json: {favorite_id: @favorite.id}
   end
 
   def destroy
     favorite = Favorite.find_by(params[:id])
     favorite.destroy
+  end
+
+  def favorites_index
+    @favorites = Favorite.where(user_id: current_user.id).order(created_at: 'DESC')
   end
 
 end
