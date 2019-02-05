11f905bb7d02876163351879faef752991eb13f0
tankazuki <34229874+tankazuki@users.noreply.github.com>
2019-02-05 12:38:33 +0900

    [Fix] カウント数を非同期で連動する処理を記述

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,15 @@ class FavoritesController < ApplicationController
   def create
     @favorite = Favorite.new(user_id: params[:user_id], camera_id: params[:camera_id])
     @favorite.save
-    render json: {favorite_id: @favorite.id}
+    favorite_count = Camera.find(params[:camera_id]).favorites_count
+    render json: {favorite_id: @favorite.id, favorite_count: favorite_count}
   end
 
   def destroy
     favorite = Favorite.find_by(params[:id])
     favorite.destroy
+    favorite_count = Camera.find(params[:camera_id]).favorites_count
+    render json: {favorite_count: favorite_count}
   end
 
   def favorites_index
